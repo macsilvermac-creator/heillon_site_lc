1eff7589f5679ab074007a174d786a7490f68cec
macsilvermac-creator <macsilver.mac@gmail.com>
2026-08-16 15:38:50 -0300

    site: coordinate corridor audio across tabs

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* HEILLON Service Worker v4
  * Network-first for HTML to avoid stale institutional pages.
  */
-const CACHE = 'heillon-v7';
+const CACHE = 'heillon-v8';
 const OFFLINE_ASSETS = ['/index.html'];
 
 self.addEventListener('install', event => {
